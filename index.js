209277b053f82a2d0d8c9e41d01f815b996a2a5f
hamzaguyo725 <hamzaguyo725@gmail.com>
2023-04-08 00:55:57 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  fetch("https://anapioficeandfire.com/api/books")
+  .then((resp) => resp.json())
+  .then((books) => renderBooks)
+   return fetch;
 }
 
 function renderBooks(books) {
@@ -14,4 +17,5 @@ function renderBooks(books) {
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
-});
+  });
+
